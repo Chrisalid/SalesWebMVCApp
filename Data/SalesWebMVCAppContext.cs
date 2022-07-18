c940480a2e9e7534c945cf58986b796eeda3fa73
Christopher Feitosa do Monte <christopher.monte@fitbank.com.br>
2022-07-18 10:37:27 -0300

    Begin of MVC Project

---
+++ Data/SalesWebMVCAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVCApp.Models;
+
+namespace SalesWebMVCApp.Data
+{
+    public class SalesWebMVCAppContext : DbContext
+    {
+        public SalesWebMVCAppContext (DbContextOptions<SalesWebMVCAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMVCApp.Models.Department> Department { get; set; }
+    }
+}
