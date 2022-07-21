cc477e35d358f6045a3166ad325d6f8bd38c538f
Christopher Feitosa do Monte <christopher.monte@fitbank.com.br>
2022-07-21 12:18:13 -0300

    add create function in seller files

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVCApp.Models;
 using SalesWebMVCApp.Services;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,19 @@ namespace SalesWebMVCApp.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -15,6 +15,12 @@ namespace SalesWebMVCApp.Services
             _context = context;
         }
 
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
