7ce29adb0677c165c64aa0489a95f4de634f9e4e
Christopher Feitosa do Monte <christopher.monte@fitbank.com.br>
2022-07-22 12:30:18 -0300

    Adicionando exceções e tela de edição

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using SalesWebMVCApp.Models;
+using SalesWebMVCApp.Services.Exceptions;
 
 namespace SalesWebMVCApp.Services
 {
@@ -37,6 +38,23 @@ namespace SalesWebMVCApp.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id Not Found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DBConcurrencyException(e.Message);
+            }
         }
     }
 
